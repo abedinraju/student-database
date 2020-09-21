328e1fde16e0bab0582cf31626a1e022d1ec830c
abedin_raju <abedin.sm@gmail.com>
2020-09-21 22:28:59 +0600

    Value get system

--- index.php
+++ index.php
@@ -9,68 +9,92 @@
 	<link rel="stylesheet" href="assets/css/responsive.css">
 </head>
 <body>
-	
+	<?php
+
+     /*
+      * Add new student form
+      */
+      if(isset($_POST['add'])){
+
+          // Get value
+
+          $name = $_POST['name'];
+          $email = $_POST['email'];
+          $cell = $_POST['cell'];
+          $uname = $_POST['uname'];
+          $age = $_POST['age'];
+
+          if (isset($_POST['gender'])){
+              $gender = $_POST['gender'];
+          }
+          $shift = $_POST['shift'];
+         echo $location = $_POST['location'];
+      }
+
+
+    ?>
 	
 
 	<div class="wrap shadow">
 		<div class="card">
 			<div class="card-body">
-				<h2>Sign Up</h2>
-				<form action="">
+				<h2>Add New Student</h2>
+				<form action="" method="POST" enctype="multipart/form-data">
 					<div class="form-group">
 						<label for="">Name</label>
-						<input class="form-control" type="text">
+						<input name="name" class="form-control" type="text">
 					</div>
 					<div class="form-group">
 						<label for="">Email</label>
-						<input class="form-control" type="text">
+						<input name="email" class="form-control" type="text">
 					</div>
 					<div class="form-group">
 						<label for="">Cell</label>
-						<input class="form-control" type="text">
+						<input name="cell" class="form-control" type="text">
 					</div>
 					<div class="form-group">
 						<label for="">Username</label>
-						<input class="form-control" type="text">
+						<input name="uname" class="form-control" type="text">
 					</div>
                     <div class="form-group">
-                        <label for="">Gender</label> <br>
-                        <input name="gender" class="" type="radio" id="male"><label for="male">Male</label>
-                        <input name="gender" class="" type="radio" id="female"><label for="female">Female</label>
+                        <label for="">Age</label>
+                        <input name="age" class="form-control" type="text">
                     </div>
                     <div class="form-group">
-                        <label for="">Age</label>
-                        <input class="form-control" type="text">
+                        <label for="">Gender</label> <br>
+                        <input name="gender" class="" type="radio" value="Male" id="male"><label for="male">Male</label>
+                        <input name="gender" class="" type="radio" value="Female" id="female"><label for="female">Female</label>
                     </div>
+
                     <div class="form-group">
                         <label for="">Shift</label>
-                        <select class="form-control" name="" id="">
+                        <select class="form-control" name="shift" id="">
                             <option value="">-Select-</option>
-                            <option value="">Day</option>
-                            <option value="">Evening</option>
+                            <option value="Day">Day</option>
+                            <option value="Evening">Evening</option>
 
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="">Location</label>
-                        <select class="form-control" name="" id="">
+                        <select class="form-control" name="location" id="">
                             <option value="">-Select-</option>
-                            <option value="">Dhaka</option>
-                            <option value="">Chittagong</option>
-                            <option value="">Rajshahi</option>
-                            <option value="">Khulna</option>
-                            <option value="">Barisal</option>
-                            <option value="">Sylhet</option>
-                            <option value="">Rongpur</option>
-                            <option value="">Mymansing</option>
+                            <option value="Dhaka">Dhaka</option>
+                            <option value="Chittagong">Chittagong</option>
+                            <option value="Rajshahi">Rajshahi</option>
+                            <option value="Khulna">Khulna</option>
+                            <option value="Barishal">Barisal</option>
+                            <option value="Sylhet">Sylhet</option>
+                            <option value="Rongpur">Rongpur</option>
+                            <option value="Mymansing">Mymansing</option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="">Photo</label>
-                        <input class="form-control-file" type="file">
+                        <input name="photo" class="form-control-file" type="file">
                     </div>
 					<div class="form-group">
-						<input class="btn btn-primary" type="submit" value="Sign Up">
+						<input name="add" class="btn btn-primary" type="submit" value="Add New Student">
 					</div>
 				</form>
 
